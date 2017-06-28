e616b7fdebf291aa5f966636806c97baa78efd7e
Thet Htut Oo <tho2410@gmail.com>
2017-06-28 16:10:52 +0630

    Links Attached

--- Gemfile
+++ Gemfile
@@ -48,6 +48,8 @@ end
 
 
 gem 'bootstrap', '~> 4.0.0.alpha6'
+gem 'simple_form'
+
 
 source 'https://rails-assets.org' do
   gem 'rails-assets-tether', '>= 1.3.3'
--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -3,4 +3,9 @@ class QuotesController < ApplicationController
 	def index
 		@quote = Quote.order("RANDOM()").first
 	end
+
+
+	def new
+		@quote = Quote.new
+	end
 end
